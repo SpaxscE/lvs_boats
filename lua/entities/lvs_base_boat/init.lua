d46fc490cefe991c6dc4600db421dd3fc957c4cd
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-28 19:09:01 +0200

    add empty scripts

---
+++ lua/entities/lvs_base_boat/init.lua
@@ -0,0 +1,5 @@
+AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "sh_camera_eyetrace.lua" )
+include("shared.lua")
+include("sh_camera_eyetrace.lua")
---
+++ lua/entities/lvs_base_boat/shared.lua
@@ -0,0 +1,14 @@
+
+ENT.Base = "lvs_base"
+
+ENT.PrintName = "[LVS] Base Boat"
+ENT.Author = "Luna"
+ENT.Information = "Luna's Vehicle Script"
+ENT.Category = "[LVS]"
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
+
+function ENT:GetVehicleType()
+	return "boat"
+end
